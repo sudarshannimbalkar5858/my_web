0b26e701d3148043f241239dca343d0a0168579f
sudarshannimbalkar5858 <sudarshannimbalkar5858@gmail.com>
2026-01-01 21:11:27 +0530

    this is first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>food-web</title>
+</head>
+<body>
+    <div class="container1">
+        <header>
+            
+        </header>
+    </div>
+</body>
+</html>
